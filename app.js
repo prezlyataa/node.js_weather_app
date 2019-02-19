8f60237b97035f4ef12be04dfab82f26c277612f
Prezlyata Yuriy <Prezlyata Yuriy>
2019-02-18 21:50:24 -0600

    adjusted method for fetch weather

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const yargs = require('yargs');
 const geocode = require('./geocode/geocode');
+const weather = require('./weather/weather');
 
 const argv = yargs
     .options({
@@ -18,6 +19,18 @@ geocode.geocodeAddress(argv.address, (errorMessage, results) => {
     if (errorMessage) {
         console.log(errorMessage);
     } else {
-        console.log(JSON.stringify(results, undefined, 2));
+        weather.fetchWeather(
+            results.latitude,
+            results.longitude,
+            (errorMessage, weatherResult) => {
+                if(errorMessage) {
+                    console.log(errorMessage)
+                } else {
+                    console.log(
+                        `Currently weather in ${JSON.stringify(results.address, undefined, 2)} - ${weatherResult.temperature}, but feels like - ${weatherResult.apparentTemperature}`
+                    );
+                }
+        });
     }
 });
+
